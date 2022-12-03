b28b9f8937e479c76ebe2b2b07dff661f3d7254b
Iambizi <amirbzmn@gmail.com>
2022-12-03 13:16:01 -0500

    First Commit

---
+++ scripts/contextMenuServiceWorker.js
@@ -0,0 +1,101 @@
+const getKey = () => {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(['openai-key'], (result) => {
+          if (result['openai-key']) {
+            const decodedKey = atob(result['openai-key']);
+            resolve(decodedKey);
+          }
+        });
+      });
+}
+
+const sendMessage = (content) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTab = tabs[0].id;
+  
+      chrome.tabs.sendMessage(
+        activeTab,
+        { message: 'inject', content },
+        (response) => {
+          if (response.status === 'failed') {
+            console.log('injection failed.');
+          }
+        }
+      );
+    });
+  };
+
+const generate = async (prompt) => {
+    // Get your API key from storage
+  const key = await getKey();
+  const url = 'https://api.openai.com/v1/completions';
+	
+  // Call completions endpoint
+  const completionResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: prompt,
+      max_tokens: 1250,
+      temperature: 0.7,
+    }),
+  });
+	
+  // Select the top choice and send back
+  const completion = await completionResponse.json();
+  return completion.choices.pop();
+}
+
+
+const generateCompletionAction = async (info) => {
+    try {
+
+        // Send mesage with generating text (this will be like a loading indicator)
+        sendMessage('generating...');
+
+        const { selectionText } = info;
+        const basePromptPrefix = `
+        Write me a detailed a media kit with the title below.
+    
+        Title:
+        `;
+    		// Add this to call GPT-3
+    const baseCompletion = await generate(`${basePromptPrefix}${selectionText}`);
+
+    // Let's see what we get!
+    console.log(baseCompletion.text)
+    
+    const secondPrompt = `
+      Take the title of the media kit below and generate a media kit written in the style of a professional PR specialist. Make it so the kit can make a sale, showcase what they offer well, and make it easy to make a sale.
+      
+      Title: ${selectionText}
+      
+      Table of Contents: ${baseCompletion.text}
+      
+      Blog Post:
+      `;
+
+    const secondPromptCompletion = await generate(secondPrompt);
+
+    // Send the output when we're all done
+    sendMessage(secondPromptCompletion.text);
+  } catch (error) {
+    console.log(error);
+
+    // Add this here as well to see if we run into any errors!
+    sendMessage(error.toString());
+  }
+};
+
+chrome.contextMenus.create({
+    id: 'context-run',
+    title: 'Generate PR content',
+    contexts: ['selection'],
+  });
+  
+//listener
+chrome.contextMenus.onClicked.addListener(generateCompletionAction);
